dcfa3cd969039f322e3ef6d25e885b35b39fef16
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2024-06-11 19:18:16 -0600

    Profile

---
+++ ReactNative/JSReact/index.js
@@ -0,0 +1,30 @@
+/*
+console.log('Hola consola')
+
+// Funciones 
+
+//Tradicional
+function suma(a,b){
+    return a+b
+}
+
+// Fat Arrow
+//const resta=(a,b)=>{ return a-b}
+
+//Fat Arrow Simplificada
+const resta=(a,b)=>a-b
+
+
+console.log(suma(1,10))
+console.log(resta(100,10))
+
+*/
+
+//Objetos
+
+const usuario={
+    nombre:'Ana',
+    apellido:'Guerra',
+    edad:30,
+    direccion:
+}
