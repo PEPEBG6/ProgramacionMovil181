5ff87716134b8cd844add65caf6e3262eedb7bcf
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2024-06-18 20:22:09 -0600

    Componentes

--- HolaMundo/App.js
+++ HolaMundo/App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+
+
+/* const Texto = (props)=>{
+  const {children} = props
+  return(
+    <Text>{ children }</Text>
+  );
+} */
+
+
+const Texto = ()=>{
+  const [contenido, setContenido]= useState('Hola :)')
+  const actualizarTexto=()=> {setContenido('contenido actualizado')}
+  return(
+    <Text onPress={actualizarTexto}> {contenido} </Text>
+  );
+}
+
 
 export default function App() {
   return (
 
     <View style={styles.container}>
-      <Text>Hola mundo ReactNative</Text>
+
+      <Texto>Uno</Texto>
+      <Texto>Dos</Texto>
+     
+
+
+      <Button title='Saludar' color="#841584" ></Button>
       <StatusBar style="auto" />
     </View>
 
